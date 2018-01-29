a30ab6a042b70694efe8bab8269fac01f2e25a9a
Rushabh Modi <modi.rushabh179@gmail.com>
2018-01-29 13:12:20 +0530

    Packaging

--- app/src/main/java/com/simform/rushabhmodi/androidlearning/activity/MainActivity.java
+++ app/src/main/java/com/simform/rushabhmodi/androidlearning/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.simform.rushabhmodi.androidlearning;
+package com.simform.rushabhmodi.androidlearning.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.simform.rushabhmodi.androidlearning.R;
 
 public class MainActivity extends AppCompatActivity {
 
