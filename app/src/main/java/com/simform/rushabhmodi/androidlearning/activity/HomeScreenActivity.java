27670c86678f4d9a2f7ac02eea3436801deba96e
Rushabh Modi <modi.rushabh179@gmail.com>
2018-01-29 17:32:17 +0530

    Navigation drawer home added

--- app/src/main/java/com/simform/rushabhmodi/androidlearning/activity/HomeScreenActivity.java
+++ app/src/main/java/com/simform/rushabhmodi/androidlearning/activity/HomeScreenActivity.java
@@ -1,8 +1,8 @@
 package com.simform.rushabhmodi.androidlearning.activity;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
 
@@ -14,12 +14,13 @@ public class HomeScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+        //TranslateAnimation translateAnimation = new TranslateAnimation(100,0,0,0);
     }
 
     public void onHomeCardClicked(View view) {
         switch (view.getId()) {
             case R.id.cardview_examples:
-                startActivity(new Intent(getIntent()));
+                startActivity(new Intent(this, HomeNavigationActivity.class));
                 break;
             default:
                 Toast.makeText(this, R.string.toast_not_ready, Toast.LENGTH_SHORT).show();
