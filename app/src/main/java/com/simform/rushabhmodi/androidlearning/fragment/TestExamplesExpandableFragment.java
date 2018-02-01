3f149286027062c2ce3b8edd70f42ead9e39b3fa
Rushabh Modi <modi.rushabh179@gmail.com>
2018-02-01 11:41:30 +0530

    no message

---
+++ app/src/main/java/com/simform/rushabhmodi/androidlearning/fragment/TestExamplesExpandableFragment.java
@@ -0,0 +1,32 @@
+package com.simform.rushabhmodi.androidlearning.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.simform.rushabhmodi.androidlearning.R;
+import com.simform.rushabhmodi.androidlearning.adapter.ErvAdapter;
+
+/**
+ * Created by rushabh.modi on 31/01/18.
+ */
+
+public class TestExamplesExpandableFragment extends Fragment {
+
+    private RecyclerView examplesExpandableRecyclerView;
+    public ErvAdapter eErvAdapter;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.base_layout_recyclerview, container, false);
+        examplesExpandableRecyclerView = view.findViewById(R.id.recyclerview_base);
+        examplesExpandableRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        return view;
+    }
+}
