f1fb195f92875a20ce7cbc17a88449781a4a0471
Rushabh Modi <modi.rushabh179@gmail.com>
2018-02-26 20:08:49 +0530

    List of files

--- app/src/main/java/com/simform/rushabhmodi/androidlearning/examples/InternalStorageExampleActivity.java
+++ app/src/main/java/com/simform/rushabhmodi/androidlearning/examples/InternalStorageExampleActivity.java
@@ -3,7 +3,10 @@ package com.simform.rushabhmodi.androidlearning.examples;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,22 +19,45 @@ import java.io.OutputStreamWriter;
 
 public class InternalStorageExampleActivity extends AppCompatActivity {
 
+    private Spinner internalSpinner;
     private EditText writeInternal;
     private TextView readInternal;
+    private FileOutputStream fileOutputStream;
+    private OutputStreamWriter outputStreamWriter;
+
+    private String fileName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_internal_storage_example);
 
+        internalSpinner = findViewById(R.id.spinner_internal_storage);
         writeInternal = findViewById(R.id.edittext_internal_write);
         readInternal = findViewById(R.id.textview_internal_read);
+
+        String[] filesList = getFilesDir().list();
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, filesList);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        internalSpinner.setAdapter(adapter);
+
+        internalSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
+                fileName = adapterView.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {}
+        });
+
     }
 
     public void onInternalWrite(View view) {
         try {
-            FileOutputStream fileOutputStream = openFileOutput(getString(R.string.internal_storage_file_name), MODE_PRIVATE);
-            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream);
+            //fileOutputStream = openFileOutput(getString(R.string.internal_storage_file_name), MODE_PRIVATE);
+            fileOutputStream = openFileOutput(fileName, MODE_PRIVATE);
+            outputStreamWriter = new OutputStreamWriter(fileOutputStream);
             outputStreamWriter.write(writeInternal.getText().toString());
             outputStreamWriter.close();
             Toast.makeText(this, R.string.internal_storage_toast_save, Toast.LENGTH_SHORT).show();
@@ -43,8 +69,8 @@ public class InternalStorageExampleActivity extends AppCompatActivity {
 
     public void onInternalAppend(View view) {
         try {
-            FileOutputStream fileOutputStream = openFileOutput(getString(R.string.internal_storage_file_name), MODE_APPEND);
-            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream);
+            fileOutputStream = openFileOutput(fileName, MODE_APPEND);
+            outputStreamWriter = new OutputStreamWriter(fileOutputStream);
             outputStreamWriter.append(writeInternal.getText().toString());
             outputStreamWriter.close();
             Toast.makeText(this, R.string.internal_storage_toast_save, Toast.LENGTH_SHORT).show();
@@ -56,19 +82,19 @@ public class InternalStorageExampleActivity extends AppCompatActivity {
 
     public void onInternalRead(View view) {
         try {
-            FileInputStream fileInputStream = openFileInput(getString(R.string.internal_storage_file_name));
+            FileInputStream fileInputStream = openFileInput(fileName);
             InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
 
             char[] inputBuffer = new char[100];
-            StringBuilder s = new StringBuilder();
+            StringBuilder stringBuilder = new StringBuilder();
             int charRead;
 
             while ((charRead = inputStreamReader.read(inputBuffer)) > 0) {
                 String readstring = String.copyValueOf(inputBuffer, 0, charRead);
-                s.append(readstring);
+                stringBuilder.append(readstring);
             }
             inputStreamReader.close();
-            readInternal.setText(s.toString());
+            readInternal.setText(stringBuilder.toString());
         } catch (Exception e) {
             e.printStackTrace();
         }
