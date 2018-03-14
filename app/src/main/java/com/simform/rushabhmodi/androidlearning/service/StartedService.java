3524b023bb4d82976cca23b38675117d2003581e
Rushabh Modi <modi.rushabh179@gmail.com>
2018-03-14 17:02:17 +0530

    Demo audio

--- app/src/main/java/com/simform/rushabhmodi/androidlearning/service/StartedService.java
+++ app/src/main/java/com/simform/rushabhmodi/androidlearning/service/StartedService.java
@@ -2,6 +2,7 @@ package com.simform.rushabhmodi.androidlearning.service;
 
 import android.app.Service;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.widget.Toast;
@@ -14,6 +15,8 @@ import com.simform.rushabhmodi.androidlearning.R;
 
 public class StartedService extends Service {
 
+    MediaPlayer mediaPlayer;
+
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
@@ -21,13 +24,21 @@ public class StartedService extends Service {
     }
 
     @Override
+    public void onCreate() {
+        //super.onCreate();
+        mediaPlayer = MediaPlayer.create(this, R.raw.demoaudio);
+    }
+
+    @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Toast.makeText(this, getString(R.string.service_btn_start), Toast.LENGTH_LONG).show();
+        mediaPlayer.start();
         return START_STICKY;
     }
 
     @Override
     public void onDestroy() {
         Toast.makeText(this, getString(R.string.service_btn_stop), Toast.LENGTH_LONG).show();
+        mediaPlayer.stop();
     }
 }
