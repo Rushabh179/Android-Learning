3048ca48ba2e9e7c958ff90a39f099771568b24b
Rushabh Modi <modi.rushabh179@gmail.com>
2018-03-19 13:16:40 +0530

    Swipe refresh layout added

--- app/src/main/java/com/simform/rushabhmodi/androidlearning/exampleactivities/OkHttpExampleActivity.java
+++ app/src/main/java/com/simform/rushabhmodi/androidlearning/exampleactivities/OkHttpExampleActivity.java
@@ -1,10 +1,12 @@
 package com.simform.rushabhmodi.androidlearning.exampleactivities;
 
 import android.support.annotation.NonNull;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.simform.rushabhmodi.androidlearning.R;
 
@@ -22,6 +24,8 @@ import okhttp3.Response;
 public class OkHttpExampleActivity extends AppCompatActivity {
 
     private ListView okHttpList;
+    private SwipeRefreshLayout okHttpSwipeRefresh;
+    //private static final String url = "https://api.androidhive.info/contacts/";
     private static final String url = "https://randomuser.me/api/";
 
     @Override
@@ -30,6 +34,21 @@ public class OkHttpExampleActivity extends AppCompatActivity {
         setContentView(R.layout.activity_web_service_example);
         if (getSupportActionBar() != null)
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        okHttpList = findViewById(R.id.listview_web_service);
+        okHttpSwipeRefresh = findViewById(R.id.swiperefresh_web_service);
+
+        okHttpSwipeRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                try {
+                    okHttpRun();
+                    okHttpSwipeRefresh.setRefreshing(false);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
 
         try {
             okHttpRun();
@@ -50,6 +69,7 @@ public class OkHttpExampleActivity extends AppCompatActivity {
 
             @Override
             public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                @SuppressWarnings("ConstantConditions")
                 final String jsonResponse = response.body().string();
                 OkHttpExampleActivity.this.runOnUiThread(new Runnable() {
                     @Override
@@ -57,6 +77,9 @@ public class OkHttpExampleActivity extends AppCompatActivity {
                         try {
                             JSONObject json = new JSONObject(jsonResponse);
                             //txtString.setText(json.getJSONObject("data").getString("first_name")+ " "+json.getJSONObject("data").getString("last_name"));
+                            Toast.makeText(OkHttpExampleActivity.this,
+                                    json.getJSONArray("results").getJSONObject(0).getString("gender"),
+                                    Toast.LENGTH_LONG).show();
 
                         } catch (JSONException e) {
                             e.printStackTrace();
