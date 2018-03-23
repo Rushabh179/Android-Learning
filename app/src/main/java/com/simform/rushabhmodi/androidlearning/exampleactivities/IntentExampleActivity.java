a00db4a89307264e0bea0e2fb3e279b0ce2bc63b
Rushabh Modi <modi.rushabh179@gmail.com>
2018-03-23 20:31:58 +0530

    Import optimisation

--- app/src/main/java/com/simform/rushabhmodi/androidlearning/exampleactivities/IntentExampleActivity.java
+++ app/src/main/java/com/simform/rushabhmodi/androidlearning/exampleactivities/IntentExampleActivity.java
@@ -4,8 +4,6 @@ import android.app.SearchManager;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
