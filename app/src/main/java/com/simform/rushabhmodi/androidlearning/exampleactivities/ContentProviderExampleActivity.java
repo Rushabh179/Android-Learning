345e7836dd5a9e00f1ca1134b5f6a592766f70f0
Rushabh Modi <modi.rushabh179@gmail.com>
2018-03-27 18:43:20 +0530

    Added content provider example

---
+++ app/src/main/java/com/simform/rushabhmodi/androidlearning/exampleactivities/ContentProviderExampleActivity.java
@@ -0,0 +1,15 @@
+package com.simform.rushabhmodi.androidlearning.exampleactivities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.simform.rushabhmodi.androidlearning.R;
+
+public class ContentProviderExampleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_content_provider_example);
+    }
+}
